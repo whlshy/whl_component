2a448a6bd6ed14e62b27fc5110b42f9ed6a63076
盧韋宏 <whl@wke.csie.ncnu.edu.tw>
2022-03-26 11:21:26 +0800

    fix

--- src/Album/Album.jsx
+++ src/Album/Album.jsx
@@ -53,7 +53,7 @@ function Album(props) {
 			}
 			!onChange && console.log('onChange', startIdx, endIdx, newimgs)
 			onChange(startIdx, endIdx, newimgs)
-			setStar(endIdx)
+			starindex == startIdx && setStar(endIdx)
 		}
 		setStartIdx(null);
 		setEndIdx(null);
